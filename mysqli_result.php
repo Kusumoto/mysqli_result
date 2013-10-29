cef6876f44a2fe96c5d1276534b7a77d3efed1e4
Azerdar Kusumoto <kusumoto.com@gmail.com>
2013-10-30 00:23:45 +0700

    Create mysqli_result.php

---
+++ mysqli_result.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+mysql_result for mysqli_result
+by :: Kusumoto
+http://kusumotolab.com
+*/
+
+//$result = mysqli data query
+//$num = data position
+//$field = get data from this field
+
+function mysqli_result($result, $num, $field) {
+    $i=0;
+    while ($row = $result->fetch_array(MYSQLI_BOTH)) {
+        if ($i==$num) $data=$row[$field];
+        $i++;
+    }
+    return $data;
+} 
+
+?>
